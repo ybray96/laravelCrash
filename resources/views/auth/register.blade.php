8811e9cb253f6f1ee814ab1887d2ba29fd6efde0
DESKTOP-C44ELP2\hp4 <dastan.ibrai@gmail.com>
2024-08-21 19:02:05 +0500

    LARAVEL 11 Crash Course for Beginners 2024 |#3 #4 Web routes (Web Developer Path)

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="title">Register a new account</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','posts.index')->name('home');
+
+Route::get('/register', function(){
+    return view('auth.register');
+})->name('register');
