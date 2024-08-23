f20d378c1a5fbebfaf87b8adffa83b92ed4b1873
DESKTOP-C44ELP2\hp4 <dastan.ibrai@gmail.com>
2024-08-23 17:10:44 +0500

    Login Users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    //Register User
     public function register(Request $request){
        //Validate
        $fields=$request->validate([
@@ -16,11 +18,37 @@ class AuthController extends Controller
         'password'=>['required','min:3','confirmed']
        ]);
        //Register
+
+       $fields['password'] = Hash::make($fields['password']);
+
        $user=User::create($fields);
 
        //Login
        Auth::login($user);
        //Redirect
        return redirect()->route('home');
+    }
+
+    //Login User
+    public function login(Request $request)
+    {
+         //Validate
+        $fields=$request->validate([            
+            'email'=>['required','max:255','email'],
+            'password'=>['required']
+           ]);
+        
+           //dd($request);
+        //Try to login the user
+        if(Auth::attempt($fields, $request->remember))
+        {
+            return redirect()->intended();
+        }
+        else
+        {
+            return back()->withErrors([
+                'failed'=>'The provided credentials do not match our records.'
+            ]);
+        }
     }
 }
